8dd48283c68f44d772aa7072f03684b7b36cd00d
TheTSD <33082152+TheTSD@users.noreply.github.com>
2017-12-20 15:15:53 +0800

    整合更新
    整合雀明华、保龄、护士、乱打

--- girl/girl.cpp
+++ girl/girl.cpp
@@ -52,6 +52,155 @@ Girl *Girl::create(Who who, int id_)
     case Id::MIHOROGI_UTA:      return new Uta(who, id);
     case Id::TAKUWA_RIO:        return new Rio(who, id);
     case Id::KAJINO_YUI:        return new Yui(who, id);
+    case Id::ARAKAWA_KEI:       return new Kei(who, id);
+    case Id::SAGIMORI_ARATA:    return new Arata(who, id);
+    case Id::CHOI_MYEONGHWA:    return new Myeonghwa(who, id);
+    case Id::SENNO_KAORI:       return new Kaori(who, id);
+    default:
+        unreached("unimplemented girl");
+    }
+}
+
+Girl *Girl::clone() const
+{
+    return new Girl(*this);
+}
+
+Girl::Girl(Who who, Id id)
+    : mSelf(who)
+    , mId(id)
+{
+}
+
+Girl::Girl(const Girl &copy)
+    : mSelf(copy.mSelf)
+    , mId(copy.mId)
+{
+}
+
+Girl::Id Girl::getId() const
+{
+    return mId;
+}
+
+void Girl::onDice(util::Rand &rand, const Table &table, Choices &choices)
+{
+    (void) rand; (void) table; (void) choices;
+}
+
+void Girl::onMonkey(std::array<Exist, 4> &exists, const Princess &princess)
+{
+    (void) exists; (void) princess;
+}
+
+bool Girl::checkInit(Who who, const Hand &init, const Princess &princess, int iter)
+{
+    (void) who; (void) init; (void) iter; (void) princess; return true;
+}
+
+void Girl::onActivate(const Table &table, Choices &choices)
+{
+    (void) table; (void) choices;
+}
+
+void Girl::onInbox(Who who, const Action &action)
+{
+    (void) who; (void) action;
+}
+
+void Girl::onDraw(const Table &table, Mount &mount, Who who, bool rinshan)
+{
+    (void) table; (void) mount; (void) who; (void) rinshan;
+}
+
+void Girl::onChooseFirstDealer(util::Rand &rand, Who tempDealer, int &die1, int &die2)
+{
+    (void) rand; (void) tempDealer; (void) die1; (void) die2;
+}
+
+void Girl::onDiscarded(const Table &table, Who who)
+{
+    (void) table; (void) who;
+}
+
+void Girl::onRiichiEstablished(const Table &table, Who who)
+{
+    (void) table; (void) who;
+}
+
+void Girl::onRoundEnded(const Table &table, RoundResult result,
+                        const std::vector<Who> &openers, Who gunner,
+                        const std::vector<Form> &fs)
+{
+    (void) table; (void) result; (void) openers; (void) gunner; (void) fs;
+}
+
+void Girl::nonMonkey(util::Rand &rand, TileCount &init, Mount &mount,
+                     std::bitset<Girl::NUM_NM_SKILL> &presence,
+                     const Princess &princess)
+{
+    (void) rand; (void) init; (void) mount; (void) presence; (void) princess;
+    unreached("unoverriden nonMonkey()");
+}
+
+const IrsCheckRow &Girl::irsCheckRow(int index) const
+{
+    (void) index;
+    unreached("unoverriden getIrsCheckRow()");
+}
+
+int Girl::irsCheckCount() const
+{
+    unreached("unoverriden irsCheckCount()");
+    return 0;
+}
+
+Choices Girl::forwardAction(const Table &table, Mount &mount, const Action &action)
+{
+    (void) table; (void) mount; (void) action;
+    unreached("unoverriden forwardAction()");
+}
+
+std::string Girl::popUpStr() const
+{
+    unreached("unoverriden popUpStr()");
+}
+
+void Girl::eraseRivered(util::Stactor<T34, 34> &ts, const River &river)
+{
+    // 34/37 does not affect equalty
+    auto has = [river](T34 t){ return util::has(river, T37(t.id34())); };
+    std::remove_if(ts.begin(), ts.end(), has);
+}
+
+void Girl::eraseRivered(std::bitset<34> &ts, const River &river)
+{
+    for (const T37 &t: river)
+        ts.reset(t.id34());
+}
+
+void Girl::accelerate(Mount &mount, const Hand &hand, const River &river, int delta)
+{
+    util::Stactor<T34, 34> effs = hand.effA4();
+
+    // do not draw dropped tiles
+    // in order to enable user-controled advance direction
+    eraseRivered(effs, river);
+
+    for (T34 t : effs)
+        mount.lightA(t, delta);
+}
+
+
+
+} // namespace saki
+
+
+
+    case Id::HONDOU_YUE:        return new Yue(who, id);
+    case Id::MIHOROGI_UTA:      return new Uta(who, id);
+    case Id::TAKUWA_RIO:        return new Rio(who, id);
+    case Id::KAJINO_YUI:        return new Yui(who, id);
     default:
         unreached("unimplemented girl");
     }
