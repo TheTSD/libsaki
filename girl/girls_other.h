614b1372ff2a85837c8d789e0242c14aac97c37f
TheTSD <33082152+TheTSD@users.noreply.github.com>
2017-12-20 15:20:16 +0800

    实装护士、乱打，南帝调整
    乱打暂时分至other

--- girl/girls_other.h
+++ girl/girls_other.h
@@ -36,6 +36,16 @@ class Rio : public Girl
 {
 public:
     GIRL_CTORS(Rio)
+    void onDraw(const Table &table, Mount &mount, Who who, bool rinshan) override;
+
+};
+
+
+
+class Kaori : public Girl
+{
+public:
+    GIRL_CTORS(Kaori)
     void onDice(util::Rand &rand, const Table &table, Choices &choices) override;
     bool checkInit(Who who, const Hand &init, const Princess &princess, int iter) override;
     void onDraw(const Table &table, Mount &mount, Who who, bool rinshan) override;
@@ -48,6 +58,46 @@ public:
 
 class Yui : public Girl
 {
+public:
+    GIRL_CTORS(Yui)
+    void onDraw(const Table &table, Mount &mount, Who who, bool rinshan) override;
+
+};
+
+
+class Kei : public Girl
+{
+public:
+    GIRL_CTORS(Kei)
+    void onDice(util::Rand &rand, const Table &table, Choices &choices) override;
+    bool checkInit(Who who, const Hand &init, const Princess &princess, int iter) override;
+    void onMonkey(std::array<Exist, 4> &exists, const Princess &princess) override;
+    void onDraw(const Table &table, Mount &mount, Who who, bool rinshan) override;
+    void onRoundEnded(const Table &table, RoundResult result,
+                      const std::vector<Who> &openers, Who gunner,
+                      const std::vector<Form> &fs) override;
+    void nonMonkey(util::Rand &rand, TileCount &init, Mount &mount,
+                            std::bitset<Girl::NUM_NM_SKILL> &presence,
+                            const Princess &princess) override;
+    int challenged = 0;
+    int fortune = 0;
+    int itcolour;
+    int midp;
+    int type;
+    private:
+    Who luckdown = mSelf;
+};
+
+
+
+} // namespace saki
+
+
+
+#endif // SAKI_GIRLS_OTHER_H
+
+
+
 public:
     GIRL_CTORS(Yui)
     void onDraw(const Table &table, Mount &mount, Who who, bool rinshan) override;
